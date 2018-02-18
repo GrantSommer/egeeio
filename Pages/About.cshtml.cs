144e8d537a123ef3e02878799bc80475273f0e30
egee-irl <brian@egee.io>
2018-02-18 13:16:46 -0800

    jesus christ git

---
+++ Pages/About.cshtml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace EgeeioWebsite.Pages
+{
+    public class AboutModel : PageModel
+    {
+        public string Message { get; set; }
+        public string Today  {get; set; }
+
+        public void OnGet()
+        {
+            Message = "Let's kick off m00ch's 2018 Jimmy Johns Tour!!!";
+            Today = DateTime.Now.ToString();
+        }
+    }
+}
